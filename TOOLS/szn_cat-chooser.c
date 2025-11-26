2c7b4a0fa7b550dd2306ac53546d8bdb34ac0686
Skyler <kesch.inno14@gmail.com>
2025-11-26 11:19:39 +0100

    Add tools and documentation for experiment

---
+++ TOOLS/szn_cat-chooser.c
@@ -0,0 +1,16 @@
+//
+// Created by i.nikiforov on 26.11.2025.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#DEFINE SZN_MAX = 41;
+#DEFINE SZN_MIN = 1;
+#DEFINE CLUE_VAL_MAX = 500;
+#DEFINE CLUE_VAL_MIN = 100;
+
+int main()
+{
+return 0;
+}
